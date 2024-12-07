32344f35063310926eeb8389f52c2ebe08a2fcc1
anastdmr <nastyakokovina26@gmail.com>
2024-12-07 23:19:58 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as divFM
+from true_math import divide as divTM
+result1 = divFM(69, 3)
+result2 = divFM(3, 0)
+result3 = divTM(49, 7)
+result4 = divTM(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
